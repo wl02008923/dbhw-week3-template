aa569fd0e8709d580aad07a379c14dd16d3f7552
unknown <>
2016-03-28 22:38:48 +0800

    1

--- single-linked-list.js
+++ single-linked-list.js
@@ -12,35 +12,70 @@ function SinglyLinkedList() {
 SinglyLinkedList.prototype.add = function(data) {
   var node = new Node(data);
   if(!this.head) {
-    //TODO
+    this.head = node;
+    this.tail = node;
   } else {
-    //TODO
+    this.tail.next = node;
+    this.tail = node;
   }
-
+  this.numberOfValues++;
 };
 
 SinglyLinkedList.prototype.remove = function(data) {
   var previous = this.head;
   var current = this.head;
-  //TODO
+  while(current){
+    if(current.data === data){
+      if(current === this.head){
+        this.head = this.head.next;
+      }if(current === this.tail){
+        this.tail = previous;
+      }else{
+        previous.next = current.next;
+      }
+      this.numberOfValues--;
+    }else{
+      previous = current;
+    }
+    current = current.next;
+  }
 };
 
 SinglyLinkedList.prototype.insertAfter = function(data, toNodeData) {
   var current = this.head;
-  //TODO
+  while(current){
+    if(current.data === toNodeData){
+      var node = new Node(data);
+      if(current === this.tail){
+        this.tail.next = node;
+        this.tail = node;
+      }else{
+        node.next = current.next;
+        current.next = node;
+      }
+      this.numberOfValues++;
+    }
+    current = current.next;
+  }
 };
 
 SinglyLinkedList.prototype.length = function() {
-  //TODO
+  return this.numberOfValues;
 };
 
 SinglyLinkedList.prototype.print = function() {
-  //TODO
+  var string = '';
+  var current = this.head;
+  while(current){
+    string += current.data + ' ';
+    current = current.next;
+  }
+  return string.trim();
 };
 
 
-/*
-singlyLinkedList.print(); // => ''
+
+/*SinglyLinkedList.print(); // => ''
 singlyLinkedList.add(1);
 singlyLinkedList.add(2);
 singlyLinkedList.add(3);
@@ -72,8 +107,8 @@ singlyLinkedList.print(); // => 2 3 4 5 6 7 8
 console.log('length is 7:', singlyLinkedList.length()); // => 7
 singlyLinkedList.print(); // => 12 13 14 15 16 17 18
 console.log('length is 7:', singlyLinkedList.length()); // => 7
-*/
 
+*/
 
 module.exports = {
   SinglyLinkedList : SinglyLinkedList,
